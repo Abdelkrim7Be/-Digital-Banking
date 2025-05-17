c6695a9d8fac60342e710d13095c31c6fd5bd6f8
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-05-17 21:33:59 +0100

    Initial Project Setup & Structure Implemented

---
+++ src/main/java/com/bellagnech/dig_bank/DigBankApplication.java
@@ -0,0 +1,13 @@
+package com.bellagnech.dig_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigBankApplication.class, args);
+	}
+
+}
