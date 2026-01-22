cc790f66a7d4dfe90282775a894a289a89dfbbc7
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2026-01-22 10:28:11 +0100

    Fix unused imports

--- microservices/account-service/src/main/java/com/bellagnech/account/clients/FeignClientConfig.java
+++ microservices/account-service/src/main/java/com/bellagnech/account/clients/FeignClientConfig.java
@@ -1,9 +1,5 @@
 package com.bellagnech.account.clients;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
-import org.springframework.cloud.openfeign.FeignClientBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
